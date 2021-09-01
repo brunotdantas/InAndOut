803196ce16983b9753179b58cdc1696e543aa4e6
Bruno Dantas <bdantas_ext@br.estee.com>
2021-09-01 10:44:32 -0300

    Fix required fields

--- Models/Item.cs
+++ Models/Item.cs
@@ -12,9 +12,14 @@ namespace InAndOut.Models
         [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Borrower { get; set; }
+
+        [Required]
         public string Lender { get; set; }
+        
         [DisplayName("Item Name")]
+        [Required]
         public string ItemName { get; set; }
     }
 }
